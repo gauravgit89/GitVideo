b95ba38bb8e6d2f68b68a532dfce5de2b9a5d75f
gitName <a@b.c>
2020-07-14 17:21:58 +0530

    Updates

---
+++ precache-manifest.43c19b55cbb67bdb76847c3c8ed9c2da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda597fd7828bd578a70b77ddbbe6228",
+    "url": "/GitVideo/index.html"
+  },
+  {
+    "revision": "6be91db59dd2d90ef6a3",
+    "url": "/GitVideo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4388da2ac38267bce1f8",
+    "url": "/GitVideo/static/js/2.87deb77b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GitVideo/static/js/2.87deb77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be91db59dd2d90ef6a3",
+    "url": "/GitVideo/static/js/main.d50d0b5f.chunk.js"
+  },
+  {
+    "revision": "be524b4f68aed9c7f9ac",
+    "url": "/GitVideo/static/js/runtime-main.dcd3e505.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GitVideo/static/media/logo.5d5d9eef.svg"
+  }
+]);
